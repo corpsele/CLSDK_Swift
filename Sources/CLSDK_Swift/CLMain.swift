ee1fb2ce0cb5b3ff6ab3896796dbcb22383df336
corpsele <corpsele555@gmail.com>
2026-02-02 09:15:44 +0800

    upgrade

--- Sources/CLSDK_Swift/CLMain.swift
+++ Sources/CLSDK_Swift/CLMain.swift
@@ -1,13 +1,13 @@
 
 import Foundation
 
-struct CLMain {
+private struct CLMain {
     public static func main() {
         print("=== this is CLSDK_Swift main Method")
     }
 }
 
-class CLMainTest {
+public class CLMainTest {
     public static func testMain() {
         print("=== this is CLSDK_Swift CLMainTest testMain Method")
     }
