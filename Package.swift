5cd3bbbcc23c6c765e09408a738ea08cda9bb4f0
corpsele <corpsele555@gmail.com>
2026-02-02 09:09:26 +0800

    upgrade

--- Package.swift
+++ Package.swift
@@ -11,15 +11,15 @@ let package = Package(
             name: "CLSDK_Swift",
             targets: ["CLSDK_Swift"]
         ),
-        .library(name: "CLSDK_Swift-Static", type: .static, targets: ["CLSDK_Swift"]),
-        .library(name: "CLSDK_Swift-Dynamic", type: .dynamic, targets: ["CLSDK_Swift"]),
+//        .library(name: "CLSDK_Swift-Static", type: .static, targets: ["CLSDK_Swift"]),
+//        .library(name: "CLSDK_Swift-Dynamic", type: .dynamic, targets: ["CLSDK_Swift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CLSDK_Swift",
-//            path: "Sources"
+            path: "Sources"
         ),
         .testTarget(
             name: "CLSDK_SwiftTests",
